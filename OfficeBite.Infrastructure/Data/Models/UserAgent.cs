6115a19cc5139e5a2760ad0f03e38030994a1340
Yordan Valkov <yordan.valkov@gmail.com>
2024-04-06 13:33:55 +0300

    Configurte UserAgent table

--- OfficeBite.Infrastructure/Data/Models/UserAgent.cs
+++ OfficeBite.Infrastructure/Data/Models/UserAgent.cs
@@ -1,6 +1,37 @@
-﻿namespace OfficeBite.Infrastructure.Data.Models
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace OfficeBite.Infrastructure.Data.Models
 {
     public class UserAgent
     {
+        [Comment("User identifier")]
+        [Key] public string UserId { get; set; } = null!;
+
+        [Comment("User First Name")]
+        [Required]
+        public string FirstName { get; set; } = string.Empty;
+
+
+        [Comment("User Last Name")]
+        [Required]
+        public string LastName { get; set; } = string.Empty;
+
+
+        [Comment("User account name")]
+        [Required]
+        public string Username { get; set; } = string.Empty;
+
+        //Adding new user features if you want...
+
+        //public string RfidCard { get; set; } = string.Empty;
+
+        [ForeignKey(nameof(UserId))]
+        public IdentityUser IdentityUser { get; set; } = null!;
+
+        public ICollection<OrderHistory> OrderHistories { get; set; } = new ICollection<OrderHistory>();
+
     }
 }
