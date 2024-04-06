7418e88316e47f54ba08ce00aebb56699109c6e2
Yordan Valkov <yordan.valkov@gmail.com>
2024-04-06 19:44:14 +0300

    Editing services

--- OfficeBite/Program.cs
+++ OfficeBite/Program.cs
@@ -1,22 +1,31 @@
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using OfficeBite.Infrastructure.Data;
+using Microsoft.AspNetCore.Localization;
+using OfficeBite.Extensions;
+using System.Globalization;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<OfficeBiteDbContext>(options =>
-    options.UseSqlServer(connectionString));
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<OfficeBiteDbContext>();
+builder.Services.AddOfficeBiteDbContext(builder.Configuration);
+builder.Services.AddOfficeBiteServices();
+builder.Services.AddApplicationIdentity(builder.Configuration);
+builder.Services.AddSeedDataLoader();
 builder.Services.AddControllersWithViews();
-
+builder.Services.ConfigureCookie(builder.Configuration);
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
+var supportedCultures = new[]
+{
+    new CultureInfo("en-US"),
+    new CultureInfo("bg-BG")
+};
+
+app.UseRequestLocalization(new RequestLocalizationOptions
+{
+    DefaultRequestCulture = new RequestCulture("bg-BG"),
+    SupportedCultures = supportedCultures,
+    SupportedUICultures = supportedCultures
+});
+
 if (app.Environment.IsDevelopment())
 {
     app.UseMigrationsEndPoint();
@@ -24,7 +33,6 @@ if (app.Environment.IsDevelopment())
 else
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -36,9 +44,7 @@ app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapDefaultControllerRoute();
 app.MapRazorPages();
 
 app.Run();
