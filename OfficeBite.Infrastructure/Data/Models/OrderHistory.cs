018b29bf265f4793e81278025a28c5f80872c501
Yordan Valkov <yordan.valkov@gmail.com>
2024-04-06 13:37:38 +0300

    Create table OrderHistory

---
+++ OfficeBite.Infrastructure/Data/Models/OrderHistory.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OfficeBite.Infrastructure.Data.Models
+{
+    public class OrderHistory
+    {
+        [Comment("Order history identifier")]
+        [Key]
+        public int Id { get; set; }
+
+        [Comment("Order id")]
+        [Required]
+        public int OrderId { get; set; }
+
+        [Comment("Connection with Id from Order table")]
+        [ForeignKey(nameof(OrderId))]
+        public Order Order { get; set; } = null!;
+    }
+}
