7c16e456fec7ae0010622dabe029be192ce8d161
Yordan Valkov <yordan.valkov@gmail.com>
2024-04-07 17:05:53 +0300

    Add dish view models

---
+++ OfficeBite.Core/Models/DishModels/DishViewModel.cs
@@ -0,0 +1,24 @@
+﻿namespace OfficeBite.Core.Models.DishModels
+{
+    public class DishViewModel
+    {
+        public int DishId { get; set; }
+
+        public string DishName { get; set; } = string.Empty;
+
+        public decimal Price { get; set; }
+        public string Description { get; set; } = string.Empty;
+
+        public bool IsVisible { get; set; }
+
+
+        public string ImageUrl { get; set; } = string.Empty;
+
+        public int CategoryId { get; set; }
+
+        public int MenuTypeId { get; set; }
+
+        public int RequestMenuNumber { get; set; }
+
+    }
+}
