5ac1a04cf102b0b0879c5399348abd28ed01cea1
Yordan Valkov <yordan.valkov@gmail.com>
2024-04-07 17:06:28 +0300

    Add Menu types view model

---
+++ OfficeBite.Core/Models/MenuModels/MenuTypeViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace OfficeBite.Core.Models.MenuModels
+{
+    public class MenuTypeViewModel
+    {
+        public int MenuTypeId { get; set; }
+
+        public string MenuName { get; set; } = string.Empty;
+    }
+}
