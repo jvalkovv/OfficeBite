710dd6e5d4fe232e823dd145d514e8097d50392c
Yordan Valkov <yordan.valkov@gmail.com>
2024-04-07 17:05:23 +0300

    Add Dish view models

---
+++ OfficeBite.Core/Models/DishModels/AllDishesViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Http;
+using OfficeBite.Core.Models.CategoryModels;
+
+namespace OfficeBite.Core.Models.DishModels
+{
+    public class AllDishesViewModel
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Име на ястието")]
+        public string DishName { get; set; } = string.Empty;
+
+        [Display(Name = "Продажна цена на ястието")]
+        public decimal DishPrice { get; set; }
+
+        [Display(Name = "Описание на ястието")]
+        public string Details { get; set; } = string.Empty;
+
+        [Display(Name = "Изберете категория на ястието")]
+        public int CategoryId { get; set; }
+
+        public bool IsVisible { get; set; }
+
+        [Display(Name = "Качете снимка на ястието")]
+        public IFormFile ImageFile { get; set; } = null!;
+
+        public IEnumerable<CategoryViewModel> Categories { get; set; } = new List<CategoryViewModel>();
+        public IEnumerable<DishViewModel> Dishes { get; set; } = new List<DishViewModel>();
+    }
+
+}
