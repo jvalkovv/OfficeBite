0dc4c370336e54b5e5d0161b5e1e3d6f4d96d269
Yordan Valkov <yordan.valkov@gmail.com>
2024-04-08 22:59:44 +0300

    Edit admin and dishe view models

--- OfficeBite.Core/Models/AdminModels/UsersViewModel.cs
+++ OfficeBite.Core/Models/AdminModels/UsersViewModel.cs
@@ -9,6 +9,8 @@ namespace OfficeBite.Core.Models.AdminModels
 
         public string UserName { get; set; } = string.Empty;
 
+        public string FullName { get; set; } = string.Empty;
+
         public string Email { get; set; } = string.Empty;
 
         public string RoleName { get; set; } = string.Empty;
--- OfficeBite.Core/Models/DishModels/DishViewModel.cs
+++ OfficeBite.Core/Models/DishModels/DishViewModel.cs
@@ -6,11 +6,10 @@
 
         public string DishName { get; set; } = string.Empty;
 
-        public decimal Price { get; set; }
+        public decimal DishPrice { get; set; }
         public string Description { get; set; } = string.Empty;
 
         public bool IsVisible { get; set; }
-
 
         public string ImageUrl { get; set; } = string.Empty;
 
