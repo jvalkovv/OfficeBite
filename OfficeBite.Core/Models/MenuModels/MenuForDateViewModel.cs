2c87c8f3df6bb30f2f2d608e34e7d4daacfb8dad
Yordan Valkov <yordan.valkov@gmail.com>
2024-04-11 21:00:27 +0300

    Add MenuForDateViewModel

---
+++ OfficeBite.Core/Models/MenuModels/MenuForDateViewModel.cs
@@ -0,0 +1,12 @@
+﻿using OfficeBite.Core.Models.DishModels;
+
+namespace OfficeBite.Core.Models.MenuModels
+{
+    public class MenuForDateViewModel
+    {
+        public int RequestMenuNumber { get; set; }
+        public decimal TotalPrice { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public List<DishViewModel> Dishes { get; set; } = new List<DishViewModel>();
+    }
+}
