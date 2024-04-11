06beb29f8d073ca2782e2799580fbb74f601a55d
Yordan Valkov <yordan.valkov@gmail.com>
2024-04-11 20:58:09 +0300

    Add AddDishToMenuViewModel

---
+++ OfficeBite.Core/Models/MenuModels/AddDishToMenuViewModel.cs
@@ -0,0 +1,22 @@
+﻿using OfficeBite.Core.Models.DishModels;
+
+namespace OfficeBite.Core.Models.MenuModels
+{
+    public class AddDishToMenuViewModel
+    {
+        public int MenuTypeId { get; set; }
+
+        public string Description { get; set; } = string.Empty;
+
+        public List<DishViewModel> AllDishes { get; set; } = new List<DishViewModel>();
+
+        public List<MenuTypeViewModel> AllMenuTypes { get; set; } = new List<MenuTypeViewModel>();
+        public List<int> SelectedDishes { get; set; } = new List<int>();
+
+        public List<DateTime> SelectedDates { get; set; } = new List<DateTime>();
+
+        public int MenuId { get; set; }
+
+        public int RequestMenuNumber { get; set; }
+    }
+}
