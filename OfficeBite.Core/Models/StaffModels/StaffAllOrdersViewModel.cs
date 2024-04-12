88682eec2802095eac7e5fcb510a1f6ee1be63bb
Yordan Valkov <yordan.valkov@gmail.com>
2024-04-12 18:53:10 +0300

    Add StaffAllOrdersViewModel

---
+++ OfficeBite.Core/Models/StaffModels/StaffAllOrdersViewModel.cs
@@ -0,0 +1,31 @@
+﻿using OfficeBite.Core.Models.DishModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OfficeBite.Core.Models.StaffModels
+{
+    public class StaffAllOrdersViewModel
+    {
+        public int OrderId { get; set; }
+
+        public string OrderName { get; set; } = string.Empty;
+        public int MenuToOrderId { get; set; }
+        public string CustomerIdentifier { get; set; } = string.Empty;
+        public string CustomerFirstName { get; set; } = string.Empty;
+
+        public string CustomerLastName { get; set; } = string.Empty;
+
+        public string CustomerUsername { get; set; } = string.Empty;
+
+        public decimal TotalPrice { get; set; }
+        public DateTime LunchDate { get; set; }
+
+        public DateTime DateOrderCreated { get; set; }
+
+        public bool IsEaten { get; set; }
+
+    }
+}
