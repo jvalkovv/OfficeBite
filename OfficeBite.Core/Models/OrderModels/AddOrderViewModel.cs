f05643541a35640c7f89f8bf3f123e5e712a94c4
Yordan Valkov <yordan.valkov@gmail.com>
2024-04-13 23:06:46 +0300

    Add OrderViewModel

---
+++ OfficeBite.Core/Models/OrderModels/AddOrderViewModel.cs
@@ -0,0 +1,21 @@
+﻿using OfficeBite.Core.Models.DishModels;
+using OfficeBite.Core.Models.MenuModels;
+
+namespace OfficeBite.Core.Models.OrderModels
+{
+    public class AddOrderViewModel
+    {
+        public int DishId { get; set; }
+        public string DishName { get; set; } = string.Empty;
+
+        public string ImageUrl { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+
+        public decimal TotalPrice { get; set; }
+
+        public List<DishViewModel> AllDishes { get; set; } = new List<DishViewModel>();
+
+        public List<MenuTypeViewModel> AllMenuTypes { get; set; } = new List<MenuTypeViewModel>();
+    }
+}
