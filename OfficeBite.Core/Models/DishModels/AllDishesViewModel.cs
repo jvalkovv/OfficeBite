13168987846edb608b3cfb3c8d120dfaa0fe146e
Yordan Valkov <yordan.valkov@gmail.com>
2024-04-13 23:05:38 +0300

    Edit alldishesviewmodel

--- OfficeBite.Core/Models/DishModels/AllDishesViewModel.cs
+++ OfficeBite.Core/Models/DishModels/AllDishesViewModel.cs
@@ -1,6 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
-using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Http;
 using OfficeBite.Core.Models.CategoryModels;
+using System.ComponentModel.DataAnnotations;
 
 namespace OfficeBite.Core.Models.DishModels
 {
