8ee3fe6581f423a9448fd5be86bb02c063188e24
Yordan Valkov <yordan.valkov@gmail.com>
2024-04-14 10:11:46 +0300

    Add Data Model Validation and ErrorMessage for AddDish

--- OfficeBite.Core/Models/DishModels/AllDishesViewModel.cs
+++ OfficeBite.Core/Models/DishModels/AllDishesViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using OfficeBite.Core.Models.CategoryModels;
 using System.ComponentModel.DataAnnotations;
+using static OfficeBite.Infrastructure.Data.Constant.DishConstants.DishConstants;
+using static OfficeBite.Core.ErrorMessages.ErrorMessage;
 
 namespace OfficeBite.Core.Models.DishModels
 {
@@ -9,12 +11,24 @@ namespace OfficeBite.Core.Models.DishModels
         public int DishId { get; set; }
 
         [Display(Name = "Име на ястието")]
+        [Required(ErrorMessage = IsRequireErrorMessage)]
+        [StringLength(DishNameMaximumLength,
+            MinimumLength = DishNameMinimumLength,
+            ErrorMessage = StringLengthErrorMessage)]
         public string DishName { get; set; } = string.Empty;
 
+
         [Display(Name = "Продажна цена на ястието")]
+        [Required(ErrorMessage = IsRequireErrorMessage)]
+        [Range(DishPriceMinimumValue, DishPriceMaximumValue, ErrorMessage = PriceValueErrorMessage)]
         public decimal DishPrice { get; set; }
 
+
         [Display(Name = "Описание на ястието")]
+        [Required(ErrorMessage = IsRequireErrorMessage)]
+        [StringLength(DishDescriptionMaximumLength,
+            MinimumLength = DishDescriptionMinimumLength,
+            ErrorMessage = StringLengthErrorMessage)]
         public string Description { get; set; } = string.Empty;
 
         [Display(Name = "Изберете категория на ястието")]
@@ -23,6 +37,8 @@ namespace OfficeBite.Core.Models.DishModels
         public bool IsVisible { get; set; }
 
         [Display(Name = "Качете снимка на ястието")]
+        [Required(ErrorMessage = IsRequireErrorMessage)]
+
         public IFormFile ImageFile { get; set; } = null!;
 
         public IEnumerable<CategoryViewModel> Categories { get; set; } = new List<CategoryViewModel>();
