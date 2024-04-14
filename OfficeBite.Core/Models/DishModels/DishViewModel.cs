b3402697a6ff2bc074dbd13e8c857afa20893c28
Yordan Valkov <yordan.valkov@gmail.com>
2024-04-14 10:12:09 +0300

    Add Data Model Validation and ErrorMessage for DishViewModel

--- OfficeBite.Core/Models/DishModels/DishViewModel.cs
+++ OfficeBite.Core/Models/DishModels/DishViewModel.cs
@@ -1,12 +1,28 @@
-﻿namespace OfficeBite.Core.Models.DishModels
+﻿using System.ComponentModel.DataAnnotations;
+using static OfficeBite.Infrastructure.Data.Constant.DishConstants.DishConstants;
+using static OfficeBite.Core.ErrorMessages.ErrorMessage;
+
+namespace OfficeBite.Core.Models.DishModels
 {
     public class DishViewModel
     {
         public int DishId { get; set; }
 
+        [Required(ErrorMessage = IsRequireErrorMessage)]
+        [StringLength(DishNameMaximumLength,
+            MinimumLength = DishNameMinimumLength,
+            ErrorMessage = StringLengthErrorMessage)]
         public string DishName { get; set; } = string.Empty;
 
+        [Required(ErrorMessage = IsRequireErrorMessage)]
+        [Range(DishPriceMinimumValue, DishPriceMaximumValue, ErrorMessage = PriceValueErrorMessage)]
+
         public decimal DishPrice { get; set; }
+
+        [Required(ErrorMessage = IsRequireErrorMessage)]
+        [StringLength(DishDescriptionMaximumLength,
+            MinimumLength = DishDescriptionMinimumLength,
+            ErrorMessage = StringLengthErrorMessage)]
         public string Description { get; set; } = string.Empty;
 
         public bool IsVisible { get; set; }
