17860d0c58c60f3abc6757ec1d44fadd84a41b5b
Yordan Valkov <yordan.valkov@gmail.com>
2024-04-14 10:10:49 +0300

    Add ErrorMessages

---
+++ OfficeBite.Core/ErrorMessages/ErrorMessage.cs
@@ -0,0 +1,9 @@
+﻿namespace OfficeBite.Core.ErrorMessages
+{
+    public class ErrorMessage
+    {
+        public const string IsRequireErrorMessage = "Полето {0} е задължително";
+        public const string StringLengthErrorMessage = "Полето {0} трябва, да е с дължина между {2} и {1} символа";
+        public const string PriceValueErrorMessage = "Полето {0} трябва, да е между {1} и {2} лева";
+    }
+}
