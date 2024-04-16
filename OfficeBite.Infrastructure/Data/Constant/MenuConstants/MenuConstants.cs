bb1a38abbf5509b2c0b9bda4096551f375a930ca
Yordan Valkov <yordan.valkov@gmail.com>
2024-04-17 01:36:39 +0300

    Add menuconstants

---
+++ OfficeBite.Infrastructure/Data/Constant/MenuConstants/MenuConstants.cs
@@ -0,0 +1,12 @@
+﻿namespace OfficeBite.Infrastructure.Data.Constant.MenuConstants
+{
+    public static class MenuConstants
+    {
+        public const int MenuOrderNameMinimumLength = 3;
+        public const int MenuOrderNameMaximumLength = 500;
+
+        public const int MenuOrderDescriptionMinimumLength = 3;
+        public const int MenuOrderDescriptionMaximumLength = 500;
+
+    }
+}
