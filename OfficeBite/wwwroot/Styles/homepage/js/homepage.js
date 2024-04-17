0977fa1c2b53653d1485acc1a9ce66f575b463c0
Yordan Valkov <yordan.valkov@gmail.com>
2024-04-17 18:27:24 +0300

    Add custom styles

---
+++ OfficeBite/wwwroot/Styles/homepage/js/homepage.js
@@ -0,0 +1,28 @@
+﻿(function () {
+
+    const select = (el, all = false) => {
+        el = el.trim()
+        if (all) {
+            return [...document.querySelectorAll(el)]
+        } else {
+            return document.querySelector(el)
+        }
+    }
+
+
+    window.addEventListener('load', () => {
+        AOS.init({
+            duration: 1000,
+            easing: 'ease-in-out',
+            once: true,
+            mirror: false
+        })
+    });
+
+    let preloader = select('#preloader');
+    if (preloader) {
+        window.addEventListener('load', () => {
+            preloader.remove()
+        });
+    }
+})()
