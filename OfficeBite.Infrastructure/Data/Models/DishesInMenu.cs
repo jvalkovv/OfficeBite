2a41333e79514d79d51e908e35b9faf9c24e9fc7
Yordan Valkov <yordan.valkov@gmail.com>
2024-04-17 21:32:43 +0300

    Edit DishInMenu

--- OfficeBite.Infrastructure/Data/Models/DishesInMenu.cs
+++ OfficeBite.Infrastructure/Data/Models/DishesInMenu.cs
@@ -4,7 +4,6 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace OfficeBite.Infrastructure.Data.Models
 {
-    //TODO.. Data validation range
     public class DishesInMenu
     {
         [Comment("Dishes In Menu Identifier")]
