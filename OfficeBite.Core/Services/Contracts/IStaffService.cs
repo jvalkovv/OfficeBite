a8ebf3a39665d0e3f35ca10d458286eb8dd20756
Yordan Valkov <yordan.valkov@gmail.com>
2024-04-23 14:29:22 +0300

    Add a service interface to the Staff controller

---
+++ OfficeBite.Core/Services/Contracts/IStaffService.cs
@@ -0,0 +1,15 @@
+﻿using OfficeBite.Core.Models.StaffModels;
+
+namespace OfficeBite.Core.Services.Contracts
+{
+    public interface IStaffService
+    {
+        Task<List<StaffAllOrdersViewModel>> AllOrders();
+
+        Task<StaffOrderViewModel?> OrderView(int id, string userId, DateTime date);
+
+        Task<StaffAllOrdersViewModel?> OrderComplete(DateTime selectedDate, string username, string userId, int orderId);
+        Task CompleteOrderConfirm(DateTime selectedDate, int orderId, string userId);
+
+    }
+}
