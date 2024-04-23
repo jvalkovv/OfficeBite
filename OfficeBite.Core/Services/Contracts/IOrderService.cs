60848ec064e7767c1d9aa370268c344e2eefa83a
Yordan Valkov <yordan.valkov@gmail.com>
2024-04-23 14:29:04 +0300

    Add a service interface to the Order controller

---
+++ OfficeBite.Core/Services/Contracts/IOrderService.cs
@@ -0,0 +1,11 @@
+﻿using OfficeBite.Core.Models.OrderModels;
+
+namespace OfficeBite.Core.Services.Contracts
+{
+    public interface IOrderService
+    {
+        Task<AddOrderViewModel> AddToOrder();
+
+        Task AddToOrder(int requestMenuNumber);
+    }
+}
