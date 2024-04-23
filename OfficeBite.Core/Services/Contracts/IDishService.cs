16aee3ce538a34dd61dc28c8ec51b11fae0a808e
Yordan Valkov <yordan.valkov@gmail.com>
2024-04-23 14:28:32 +0300

    Add a service interface to the Dish controller

---
+++ OfficeBite.Core/Services/Contracts/IDishService.cs
@@ -0,0 +1,33 @@
+﻿using OfficeBite.Core.Models.DishModels;
+
+namespace OfficeBite.Core.Services.Contracts
+{
+    public interface IDishService
+    {
+        Task<AllDishesViewModel> GetAllDishes();
+
+        Task<AllDishesViewModel> GetAllHiddenDishes();
+
+        Task<DishViewModel?> HideDish(int dishId);
+
+        Task HideDishConfirm(int dishId);
+
+        Task<DishViewModel?> UnHideDish(int dishId);
+
+        Task UnHideDishConfirm(int dishId);
+
+        Task<AllDishesViewModel> EditDish(int dishId);
+
+
+        Task<AllDishesViewModel> EditDish(AllDishesViewModel model, int dishId);
+
+
+        Task<AllDishesViewModel> AddDish();
+
+        Task<AllDishesViewModel> AddDish(AllDishesViewModel model);
+
+        Task<DishViewModel?> DeleteDish(int dishId);
+
+        Task DeleteDishConfirm(int dishId);
+    }
+}
