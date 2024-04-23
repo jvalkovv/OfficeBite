37889490a505d28322c86e741bc9f7d75e4a4d6e
Yordan Valkov <yordan.valkov@gmail.com>
2024-04-23 14:28:46 +0300

    Add a service interface to the Menu controller

---
+++ OfficeBite.Core/Services/Contracts/IMenuService.cs
@@ -0,0 +1,19 @@
+﻿using OfficeBite.Core.Models.MenuModels;
+
+namespace OfficeBite.Core.Services.Contracts
+{
+    public interface IMenuService
+    {
+        Task<MenuViewModel> GetMenuListAsync();
+
+        Task<MenuDailyViewModel> GetMenuDailyListAsync();
+
+        Task<MenuDailyViewModel> MenuDailyList(MenuDailyViewModel model);
+
+        Task<AddDishToMenuViewModel> GetMenuAddDishToMenuListAsync();
+
+        Task<AddDishToMenuViewModel> AddDishToMenuListAsync(AddDishToMenuViewModel model);
+
+
+    }
+}
