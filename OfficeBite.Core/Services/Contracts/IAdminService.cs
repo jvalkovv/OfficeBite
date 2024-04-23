77f6852375232f114da1bb4b046721df0bfca69b
Yordan Valkov <yordan.valkov@gmail.com>
2024-04-23 14:28:14 +0300

    Add a service interface to the admin controller

---
+++ OfficeBite.Core/Services/Contracts/IAdminService.cs
@@ -0,0 +1,21 @@
+﻿using OfficeBite.Core.Models.AdminModels;
+
+namespace OfficeBite.Core.Services.Contracts
+{
+    public interface IAdminService
+    {
+        Task<List<RoleViewModel>> GetUserRoles();
+
+        Task<List<UsersViewModel>> GetUsers();
+
+        Task<AdminPanelViewModel> Admin();
+
+
+        Task AssignRole(AdminPanelViewModel model);
+
+        Task DeleteRole(AdminPanelViewModel model);
+
+        Task DeleteUser(AdminPanelViewModel model);
+
+    }
+}
