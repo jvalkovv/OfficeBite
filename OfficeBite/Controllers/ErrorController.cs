204deb4a4bfcf27b0e0cb39172d63d0ec249f60f
Yordan Valkov <yordan.valkov@gmail.com>
2024-04-23 16:46:07 +0300

    Add notfound error controller

---
+++ OfficeBite/Controllers/ErrorController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OfficeBite.Controllers
+{
+    public class ErrorController : Controller
+    {
+        [Route("/error/404")]
+        public IActionResult Error404()
+        {
+            return View("NotFound");
+        }
+    }
+}
