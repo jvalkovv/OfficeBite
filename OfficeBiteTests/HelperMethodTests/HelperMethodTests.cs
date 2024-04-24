bc47469fe56544a79557d303f185496e1114384e
Yordan Valkov <yordan.valkov@gmail.com>
2024-04-24 18:51:09 +0300

    NUnit test for helpermethods

--- OfficeBiteTests/HelperMethodTests/HelperMethodTests.cs
+++ OfficeBiteTests/HelperMethodTests/HelperMethodTests.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using NUnit.Framework.Legacy;
-using OfficeBite.Extensions;
+using OfficeBite.Core.Extensions;
 using OfficeBite.Infrastructure.Data;
 using OfficeBite.Infrastructure.Data.Models;
 
@@ -15,13 +15,12 @@ namespace OfficeBiteTests.HelperMethodTests
         [SetUp]
         public void Setup()
         {
-            // Инициализация на контекста на базата данни в паметта
             var options = new DbContextOptionsBuilder<OfficeBiteDbContext>()
                 .UseInMemoryDatabase(databaseName: "TestDatabase")
                 .Options;
             _dbContext = new OfficeBiteDbContext(options);
 
-            // Попълване на базата данни с фалшиви данни за тестовете
+        
             _dbContext.DishCategories.AddRange(new List<DishCategory>
             {
                 new DishCategory { Id = 1, Name = "Category 1" },
@@ -36,28 +35,27 @@ namespace OfficeBiteTests.HelperMethodTests
 
             _dbContext.SaveChanges();
 
-            // Инициализация на HelperMethods с мокнат контекст на базата данни
+       
             _helperMethods = new HelperMethods(_dbContext);
         }
 
         [TearDown]
         public void TearDown()
         {
-            // Почистване на ресурсите
+       
             _dbContext.Dispose();
         }
 
         [Test]
         public async Task GetDishForMenuAsync_ReturnsCorrectDishViewModels()
         {
-            // Act
+       
             var result = await _helperMethods.GetDishForMenuAsync();
 
-            // ClassicAssert
+      
             ClassicAssert.IsNotNull(result);
             ClassicAssert.AreEqual(2, result.Count());
 
-            // Проверка на правилността на върнатите модели за ястия
             var dishViewModel1 = result.FirstOrDefault(d => d.DishId == 1);
             ClassicAssert.IsNotNull(dishViewModel1);
             ClassicAssert.AreEqual("Dish 1", dishViewModel1.DishName);
