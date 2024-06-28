2cb31cffaa368ed8e27cfca42635b43b4fe1180b
Yordan Valkov <yordan.valkov@gmail.com>
2024-06-28 20:43:42 +0300

    Remove capture implementation

--- OfficeBite/Controllers/HomeController.cs
+++ OfficeBite/Controllers/HomeController.cs
@@ -15,32 +15,5 @@ namespace OfficeBite.Controllers
         {
             return View();
         }
-
-        [HttpPost]
-        public IActionResult UploadImage(IFormFile imageFile)
-        {
-            if (imageFile == null || imageFile.Length == 0)
-            {
-                return BadRequest("No image file received");
-            }
-
-            var fileName = Guid.NewGuid() + Path.GetExtension(imageFile.FileName);
-            var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "UploadedImages", fileName);
-
-       
-            Directory.CreateDirectory(Path.GetDirectoryName(filePath));
-
-            using (var stream = new FileStream(filePath, FileMode.Create))
-            {
-                imageFile.CopyTo(stream);
-            }
-
-            return Content("Image uploaded successfully");
-        }
-
-        public IActionResult CaptureImage()
-        {
-            return View();
-        }
     }
 }
